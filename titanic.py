b83b382235502bd4982c0faee8957282ac191209
syarifah84 <103180171+syarifah84@users.noreply.github.com>
2022-04-10 22:47:56 +0800

    Update titanic.py

--- titanic.py
+++ titanic.py
@@ -68,4 +68,4 @@ hide_streamlit_style = """
             footer {visibility: hidden;}
             </style>
             """
-st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
